0dd8653f2abe84a93759246d45fa2265f253460a
zhangzhiwei <angtylook@126.com>
2016-12-31 17:37:46 +0800

    part test

---
+++ part.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+// return index in arr, where [start, index) less than num
+// [index, end) great or equal num.
+size_t part(const vector<int>& arr, size_t start, size_t end, int num)
+{
+    size_t mid = (start + end) / 2;
+    int mid_value = arr[mid];
+    if(num < mid_value) {
+        return part(arr, start, mid, num)
+    }
+    if(num > mid_value) {
+        return part(arr, 
+    }
+
+int main(int argc, const char* argv[])
+{
+    return 0;
+}
