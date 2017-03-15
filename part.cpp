7c72bf968e78913e89ec41b945d77d4ddf4f9948
jxunhappy <lookto2008@gmail.com>
2017-03-15 23:34:44 +0800

    add vscode and make part.cpp buildable.

--- part.cpp
+++ part.cpp
@@ -3,16 +3,18 @@
 
 // return index in arr, where [start, index) less than num
 // [index, end) great or equal num.
-size_t part(const vector<int>& arr, size_t start, size_t end, int num)
+size_t part(const std::vector<int>& arr, size_t start, size_t end, int num)
 {
     size_t mid = (start + end) / 2;
     int mid_value = arr[mid];
     if(num < mid_value) {
-        return part(arr, start, mid, num)
+        return part(arr, start, mid, num);
     }
     if(num > mid_value) {
-        return part(arr, 
+        return part(arr, mid, end, num);
     }
+    return mid;
+}
 
 int main(int argc, const char* argv[])
 {
