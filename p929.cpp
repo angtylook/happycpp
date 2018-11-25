5eceedaddfab89ba9c9d6b5663d604b6044d0c37
jxunhappy <lookto2008@gmail.com>
2018-11-25 16:25:00 +0800

    delete unused code

--- p929.cpp
+++ p929.cpp
@@ -1,40 +1,6 @@
 #include <iostream>
-#include <filesystem>
-#include <regex>
-#include <fstream>
-#include <system_error>
-#include <limits>
-#include <time.h>
-#include <iterator>
 #include <set>
-
-void process_file(std::filesystem::path file, std::ofstream& out, std::regex& re) {
-	std::cout << "file: " << file << std::endl;
-	out << "file: " << file << std::endl;
-	std::ifstream in(file);
-	std::string line;
-	while (std::getline(in, line)) {
-		auto str_begin = std::sregex_token_iterator(line.begin(), line.end(), re, 1);
-		auto str_end = std::sregex_token_iterator();
-		for (auto i = str_begin; i != str_end; ++i) {
-			out << i->str() << std::endl;
-			std::cout << i->str() << std::endl;
-		}
-	}
-	in.close();
-}
-
-void findStr() {
-	std::regex re{ R"d(fmt.Errorf\("(.+)")d" };
-	std::ofstream out("str.txt");
-	for (auto entry : std::filesystem::recursive_directory_iterator(std::filesystem::path("G:/devsrc/little_games/server_go/src/ateam/room"))) {
-		auto path = entry.path();
-		if (std::filesystem::is_regular_file(path) && path.extension() == ".go") {
-			process_file(path, out, re);
-		}
-	}
-	out.close();
-}
+#include <vector>
 
 class Solution {
  public:
