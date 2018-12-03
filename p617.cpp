164e29865ae77c2e86e7a6eebbb09104abca5632
jxunhappy <lookto2008@gmail.com>
2018-12-03 23:19:46 +0800

    p617

---
+++ p617.cpp
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) { return t1; }
+};
+
+int main() {}
