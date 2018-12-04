56e64124d5ff5d878bb3b44c09e8d8da634ad7e6
jxunhappy <lookto2008@gmail.com>
2018-12-04 23:49:20 +0800

    solve p617

--- p617.cpp
+++ p617.cpp
@@ -10,7 +10,18 @@ struct TreeNode {
 
 class Solution {
 public:
-    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) { return t1; }
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if (t1 == nullptr && t2 == nullptr)
+            return nullptr;
+        if (t1 != nullptr && t2 == nullptr)
+            return t1;
+        if (t1 == nullptr && t2 != nullptr)
+            return t2;
+        t1->left = mergeTrees(t1->left, t2->left);
+        t1->right = mergeTrees(t1->right, t2->right);
+        t1->val = t1->val + t2->val;
+        return t1;
+    }
 };
 
 int main() {}
