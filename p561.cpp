767c5697ed2aa87ac751eda84019a07fc78991f4
jxunhappy <lookto2008@gmail.com>
2018-12-08 03:00:18 +0800

    solve p561

--- p561.cpp
+++ p561.cpp
@@ -1,45 +1,16 @@
 #include <algorithm>
 #include <cassert>
+#include <iostream>
 #include <vector>
 
 class Solution {
 public:
     int arrayPairSum(std::vector<int>& nums) {
-        size_t n = nums.size();
-        int min_num = nums[0];
-        int max_num = nums[1];
-        if (min_num > max_num) {
-            min_num = nums[1];
-            max_num = nums[0];
-        }
-        int sum = min_num;
-
-        for (size_t i = 2; i < n; i += 2) {
-            auto small = nums[i];
-            auto big = nums[i + 1];
-            if (big < small) {
-                small = nums[i + 1];
-                big = nums[i];
-            }
-
-            if (small >= max_num) {
-                sum += small;
-                max_num = big;
-                continue;
-            }
-
-            if (big <= min_num) {
-                sum += small;
-                min_num = small;
-                continue;
-            }
-
-            if (max_num < big) {
-                sum += max_num;
-                max_num = big;
-            } else {
-                sum += big;
-            }
+        std::sort(nums.begin(), nums.end());
+        auto n = nums.size();
+        int sum{0};
+        for (size_t i = 0; i < n; i += 2) {
+            sum += nums[i];
         }
         return sum;
     }
@@ -51,6 +22,6 @@ int main() {
     assert(sol.arrayPairSum(vi) == 4);
 
     std::vector<int> vi1{7, 3, 1, 0, 0, 6};
-    assert(sol.arrayPairSum(vi) == 7);
+    assert(sol.arrayPairSum(vi1) == 7);
     return 0;
 }
