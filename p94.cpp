09bc370c2a4e40a2cc3d8e8373be1a754f91f9a6
jxunhappy <lookto2008@gmail.com>
2018-12-16 00:15:38 +0800

    solve p94

--- p94.cpp
+++ p94.cpp
@@ -1,3 +1,7 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <stack>
 #include <vector>
 
 // Definition for a binary tree node.
@@ -10,7 +14,34 @@ struct TreeNode {
 
 class Solution {
 public:
-    std::vector<int> inorderTraversal(TreeNode* root) {}
+    std::vector<int> inorderTraversal(TreeNode* root) {
+        std::vector<int> result;
+        std::stack<TreeNode*> s;
+        while (root != nullptr || !s.empty()) {
+            if (root != nullptr) {
+                s.push(root);
+                root = root->left;
+            } else {
+                root = s.top();
+                s.pop();
+                result.push_back(root->val);
+                root = root->right;
+            }
+        }
+        return result;
+    }
 };
 
-int main() {}
+int main() {
+    auto root = new TreeNode(1);
+    // root->left = new TreeNode(4);
+    root->right = new TreeNode(2);
+    root->right->left = new TreeNode(3);
+
+    Solution sol;
+    auto vi = sol.inorderTraversal(root);
+    std::ostream_iterator<int> out(std::cout, " ");
+    std::copy(vi.begin(), vi.end(), out);
+    std::cout << std::endl;
+    return 0;
+}
