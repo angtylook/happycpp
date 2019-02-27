74171a6a129dac73a7d116a54e9106201f7665f3
jxunhappy <lookto2008@gmail.com>
2019-02-27 21:51:51 +0800

    base

---
+++ base/util.h
@@ -0,0 +1,17 @@
+#ifndef UTIL_H_
+#define UTIL_H_
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const std::vector<T>& v) {
+    out << "[";
+    std::ostream_iterator<T> out_iter(out, ", ");
+    std::copy(v.begin(), v.end(), out_iter);
+    out << "]";
+    return out;
+}
+
+#endif UTIL_H_
