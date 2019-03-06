8df1d36a108d269d06def023c6c8453d1572c4c7
jxunhappy <lookto2008@gmail.com>
2019-03-07 00:05:43 +0800

    p136

--- p136.cpp
+++ p136.cpp
@@ -1,13 +1,21 @@
-#include <vector>
+#include <algorithm>
 #include <iostream>
+#include <vector>
 
 class Solution {
 public:
     int singleNumber(std::vector<int>& nums) {
-        return 0;
+        int sum = 0;
+        std::for_each(nums.begin(), nums.end(), [&sum](int v) { sum ^= v; });
+        return sum;
     }
 };
 
 int main() {
+    Solution sol;
+    std::vector t1{2, 2, 1};
+    std::cout << sol.singleNumber(t1) << std::endl;
+    std::vector t2{4, 1, 2, 1, 2};
+    std::cout << sol.singleNumber(t2) << std::endl;
     return 0;
 }
