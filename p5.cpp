f366c9ded6608429ec1034a3eeaade12bd10f143
jxunhappy <lookto2008@gmail.com>
2019-03-15 01:46:27 +0800

    p5

--- p5.cpp
+++ p5.cpp
@@ -11,42 +11,59 @@ public:
 
         cache_.clear();
         std::string_view v(s);
-        auto p = longestPalindrome(v, 0, v.size());
+        auto p = longestPalindrome(v);
         return std::string(p);
     }
 
-    std::string_view longestPalindrome(std::string_view v,
-                                       size_t first,
-                                       size_t last) {
-        std::pair rng{first, last};
-        auto iter = cache_.find(rng);
+    std::string longestPalindrome2(std::string s) {
+        std::string_view v(s);
+        std::string_view ret(v.substr(0, 1));
+        auto vz = v.size();
+        for (size_t i = 1; i < vz; i++) {
+            auto sz = ret.size();
+            auto test_v = v.substr(i - sz, sz + 1);
+            if (isPalindrome(test_v)) {
+                ret = test_v;
+            }
+
+            if (i - sz >= 1) {
+                auto test_v = v.substr(i - sz - 1, sz + 2);
+                if (isPalindrome(test_v)) {
+                    ret = test_v;
+                }
+            }
+        }
+        return std::string(ret);
+    }
+
+    std::string_view longestPalindrome(std::string_view v) {
+        auto iter = cache_.find(v);
         if (iter != cache_.end()) {
             return iter->second;
         }
 
-        auto test_v = v.substr(first, last);
-        m[test_v]++;
-        if (isPalindrome(test_v)) {
-            cache_[rng] = test_v;
-            return test_v;
+        m[v]++;
+        if (isPalindrome(v)) {
+            cache_[v] = v;
+            return v;
         }
 
-        std::pair lrng{first, last - 1};
         std::string_view left;
-        iter = cache_.find(lrng);
+        auto lv = v.substr(0, v.size() - 1);
+        iter = cache_.find(lv);
         if (iter == cache_.end()) {
-            left = longestPalindrome(v, first, last - 1);
-            cache_[lrng] = left;
+            left = longestPalindrome(lv);
+            cache_[lv] = left;
         } else {
             left = iter->second;
         }
 
-        std::pair rrng{first + 1, last};
         std::string_view right;
-        iter = cache_.find(rrng);
+        auto rv = v.substr(1, v.size());
+        iter = cache_.find(rv);
         if (iter == cache_.end()) {
-            right = longestPalindrome(v, first + 1, last);
-            cache_[rrng] = right;
+            right = longestPalindrome(rv);
+            cache_[rv] = right;
         } else {
             right = iter->second;
         }
@@ -56,7 +73,7 @@ public:
             ret = right;
         }
 
-        cache_[rng] = right;
+        cache_[v] = ret;
         return ret;
     }
 
@@ -73,18 +90,20 @@ public:
 
     void stat() {
         for (auto [v, n] : m) {
-            std::cout << v << ":" << n << std::endl;
+            if (n > 1) {
+                std::cout << v << ":" << n << std::endl;
+            }
         }
     }
 
 private:
     std::map<std::string_view, int> m;
-    std::map<std::pair<int, int>, std::string_view> cache_;
+    std::map<std::string_view, std::string_view> cache_;
 };
 
 int main() {
     Solution sol;
-    std::cout << sol.longestPalindrome(std::string("babad")) << std::endl;
+    std::cout << sol.longestPalindrome2(std::string("babad")) << std::endl;
     std::cout << "=============" << std::endl;
     sol.stat();
     std::cout
@@ -106,7 +125,27 @@ int main() {
                    "tfyirnhkrhbrwkdymjrjklonyggqnxhfvtkqxoicakzsxmgczpwhpkzcntk"
                    "cwhkdkxvfnjbvjjoumczjyvdgkfukfuldolqnauvoyhoheoqvpwoisniv"))
         << std::endl;
-    std::cout << "=============" << std::endl;
+    std::cout << "===qahaq==========" << std::endl;
     sol.stat();
+    std::cout
+        << sol.longestPalindrome2(
+               // std::string("babaddtattarrattatddetartrateedredividerb"))
+               std::string(
+                   "kyyrjtdplseovzwjkykrjwhxquwxsfsorjiumvxjhjmgeueafubtonhlerr"
+                   "gsgohfosqssmizcuqryqomsipovhhodpfyudtusjhonlqabhxfahfcjqxyc"
+                   "kycstcqwxvicwkjeuboerkmjshfgiglceycmycadpnvoeaurqatesivajoq"
+                   "dilynbcihnidbizwkuaoegmytopzdmvvoewvhebqzskseeubnretjgnmyjw"
+                   "wgcooytfojeuzcuyhsznbcaiqpwcyusyyywqmmvqzvvceylnuwcbxybhqpv"
+                   "jumzomnabrjgcfaabqmiotlfojnyuolostmtacbwmwlqdfkbfikusuqtupd"
+                   "wdrjwqmuudbcvtpieiwteqbeyfyqejglmxofdjksqmzeugwvuniaxdrunyu"
+                   "nnqpbnfbgqemvamaxuhjbyzqmhalrprhnindrkbopwbwsjeqrmyqipnqvjq"
+                   "zpjalqyfvaavyhytetllzupxjwozdfpmjhjlrnitnjgapzrakcqahaqetwl"
+                   "laaiadalmxgvpawqpgecojxfvcgxsbrldktufdrogkogbltcezflyctklpq"
+                   "rjymqzyzmtlssnavzcquytcskcnjzzrytsvawkavzboncxlhqfiofuoheha"
+                   "ygxidxsofhmhzygklliovnwqbwwiiyarxtoihvjkdrzqsnmhdtdlpckuayh"
+                   "tfyirnhkrhbrwkdymjrjklonyggqnxhfvtkqxoicakzsxmgczpwhpkzcntk"
+                   "cwhkdkxvfnjbvjjoumczjyvdgkfukfuldolqnauvoyhoheoqvpwoisniv"))
+        << std::endl;
+
     return 0;
 }
