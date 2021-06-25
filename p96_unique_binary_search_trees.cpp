a94a5d8d5b13a78ab7ac84d8ee944c1b08b6c705
happylife <lookto2008@gmail.com>
2021-06-26 04:13:02 +0800

    p96 opt

--- p96_unique_binary_search_trees.cpp
+++ p96_unique_binary_search_trees.cpp
@@ -7,22 +7,18 @@
 class Solution {
 public:
     int numTrees(int n) {
-        switch (n) {
-            case 0:
-                return 0;
-            case 1:
-                return 1;
-            case 2:
-                return 2;
-            case 3:
-                return 5;
+        if (n < nums_.size()) {
+            return nums_[n];
         }
         int total = 0;
         for (int i = 1; i <= n; i++) {
             total += std::max(1, numTrees(i-1)) * std::max(1, numTrees(n - i));
         }
+        nums_.push_back(total);
         return total;
     }
+private:
+    std::vector<int> nums_ = {0, 1, 2, 5};
 };
 
 int main() {
